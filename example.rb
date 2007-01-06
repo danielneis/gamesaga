9823109d720091b96d4e32b2dd26285698c6cb80
Daniel Neis <danielneis@gmail.com>
2007-01-06 16:17:47 +0000

    Agora com movimentos controlados pelo teclado

--- example.rb
+++ example.rb
@@ -3,7 +3,7 @@ require 'rubygame'
 Rubygame.init()
 
 size = [640, 480]
-speed = [2, 2]
+speed = 10
 black = 0, 0, 0
 
 Rubygame.init()
@@ -21,7 +21,29 @@ class Panda
 	super()
 	@image = @@pandapic
 	@rect = Rubygame::Rect.new(x,y,*@@pandapic.size)
+	# @area is the area of the screen, which the panda will walk across
+	@area = Rubygame::Rect.new(0,0,[640,480])
+
+	@xvel = 9 # called self.move in the pygame example
     end
+
+    def walk(x,y)
+	newpos = @rect.move(x,y)
+	# If the panda starts to walk off the screen
+	
+	#if (@rect.left < @area.left) or (@rect.right > @area.right)
+	#    @xvel = -@xvel		# reverse direction of movement
+	#    newpos = @rect.move(@xvel,0) # recalculate with changed velocity
+	#    @image = Rubygame::Transform.flip(@image, true, false) # flip x
+	#end
+	@rect = newpos
+    end
+
+    def update(x,y)
+	walk(x,y)
+    end
+
+    private :walk
 
 end
 
@@ -34,13 +56,11 @@ screen = Rubygame::Screen.set_mode(size)
 screen.set_caption("teste do Rubygame")
 
 # Create the very cute panda objects!
-panda1 = Panda.new(10,10)
-panda2 = Panda.new(50,50)
-panda3 = Panda.new(100,100)
+panda1 = Panda.new(300,150)
 
 # Create the grou and put the pandas on it
 pandas = PandaGroup.new
-pandas.push(panda1,panda2,panda3)
+pandas.push(panda1)
 
 # Make the background surface
 background = Rubygame::Surface.new(screen.size)
@@ -57,10 +77,18 @@ catch(:rubygame_quit)    do
 		    case event.key
 			when Rubygame::K_ESCAPE
 			    throw :rubygame_quit 
+			when Rubygame::K_UP
+			    pandas.update(0,-speed)
+			when Rubygame::K_DOWN
+			    pandas.update(0,speed)
+			when Rubygame::K_LEFT
+			    pandas.update(-speed,0)
+			when Rubygame::K_RIGHT
+			    pandas.update(speed,0)
 		    end
 	    end
 	end
-	pandas.undraw(screen,background)
+	background.blit(screen, [0, 0])
 	pandas.draw(screen)
 	screen.update()
     end
