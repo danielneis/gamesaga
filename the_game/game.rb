3f8a8d72f7d617931251db1d0e33295424354dfe
Daniel Neis <danielneis@gmail.com>
2007-01-07 18:44:45 +0000

    Implementando um sistema de contagem de FPS. (Precisa ser melhorado)

--- the_game/game.rb
+++ the_game/game.rb
@@ -5,17 +5,25 @@ require 'player.class.rb'
 
 Rubygame.init()
 
-queue = Rubygame::Queue.instance
 
-# Create the SDL window
+# Create the SDL window, event queue and fps clock
 size = [SCREEN_WIDTH, SCREEN_HEIGHT]
-screen = Rubygame::Screen.set_mode(size)
-screen.set_caption("Joguinho...")
+screen = Rubygame::Screen.set_mode(SCREEN_SIZE)
+screen.set_caption(TITLE)
+
+queue = Rubygame::Queue.instance
+clock = Rubygame::Time::Clock.new()
+
+# Create the life bar, FPS display etc.
+life =  nil
+sfont = Rubygame::SFont.new("term16.png")
+fps_display = sfont.render('FPS: '+clock.fps.to_s)
+fps_display.blit(screen, [0,0])
 
 # Create the player character
 player = Player.new()
 
-# Create the group and put the characters on it
+# Create the group and put the everything needed on it
 allsprites = Rubygame::Sprites::Group.new()
 allsprites.push(player)
 
@@ -23,49 +31,51 @@ allsprites.push(player)
 background = Rubygame::Image.load('castle.png')
 background.blit(screen,[0,0])
 
-catch(:rubygame_quit)    do
-    loop do
-	queue.get().each do |event|
-	    case event
-		when Rubygame::QuitEvent
-		    throw :rubygame_quit
-		when Rubygame::KeyDownEvent
-		    case event.key
-			when Rubygame::K_ESCAPE
-			    throw :rubygame_quit 
-			when Rubygame::K_UP
-			    player.set_state(:up)
-			when Rubygame::K_DOWN
-			    player.set_state(:down)
-			when Rubygame::K_LEFT
-			    player.set_state(:left)
-			when Rubygame::K_RIGHT
-			    player.set_state(:right)
-		    end
-		when Rubygame::KeyUpEvent
-		    case event.key
-			when Rubygame::K_UP 
-			    if  player.state == :up then
-				player.set_state(:still)
-			    end
-			when Rubygame::K_DOWN
-			    if  player.state == :down then
-				player.set_state(:still)
-			    end
-			when Rubygame::K_LEFT
-			    if  player.state == :left then
-				player.set_state(:still)
-			    end
-			when Rubygame::K_RIGHT
-			    if  player.state == :right then
-				player.set_state(:still)
-			    end
-		    end
-	    end
+#Main Loop
+loop do
+    clock.tick()
+    queue.get().each do |event|
+	case event
+	    when Rubygame::QuitEvent
+		exit
+	    when Rubygame::KeyDownEvent
+		case event.key
+		    when Rubygame::K_ESCAPE
+			exit
+		    when Rubygame::K_UP
+			player.set_state(:up)
+		    when Rubygame::K_DOWN
+			player.set_state(:down)
+		    when Rubygame::K_LEFT
+			player.set_state(:left)
+		    when Rubygame::K_RIGHT
+			player.set_state(:right)
+		end
+	    when Rubygame::KeyUpEvent
+		case event.key
+		    when Rubygame::K_UP 
+			if  player.state == :up then
+			    player.set_state(:still)
+			end
+		    when Rubygame::K_DOWN
+			if  player.state == :down then
+			    player.set_state(:still)
+			end
+		    when Rubygame::K_LEFT
+			if  player.state == :left then
+			    player.set_state(:still)
+			end
+		    when Rubygame::K_RIGHT
+			if  player.state == :right then
+			    player.set_state(:still)
+			end
+		end
 	end
-	background.blit(screen, [0, 0])
-	allsprites.update
-	allsprites.draw(screen)
-	screen.update()
     end
-end    
+    background.blit(screen, [0, 0])
+    fps_display = sfont.render('FPS: '+clock.fps.to_s)
+    fps_display.blit(screen, [0,0])
+    allsprites.update
+    allsprites.draw(screen)
+    screen.update()
+end
