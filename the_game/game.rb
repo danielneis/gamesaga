41b19f530771fa79c62a36291bc750962e7f7b94
Daniel Neis <danielneis@gmail.com>
2007-02-21 21:51:39 +0000

    Transformando o módulo 'game' numa classe e passando todos os contextos para um mesmo arquivo. (Ainda não está bom, para variar, mas é um primeiro passo para um melhor organização)

--- the_game/game.rb
+++ the_game/game.rb
@@ -9,21 +9,20 @@ require 'character'
 require 'player'
 require 'enemy'
 require 'items'
-require 'contexts/main_menu'
-require 'contexts/main'
-require 'contexts/pause'
-require 'contexts/player_death'
+require 'main'
 
 # Initialize rubygame, set up screen and start the event queue
 Rubygame.init()
 screen = Rubygame::Screen.set_mode(SCREEN_SIZE)
 screen.set_caption(TITLE)
 queue = Rubygame::Queue.instance
+Game.new()
 
 catch(:end_game) do
-  loop do
-    Game::MainMenu(screen, queue)
 
-    Game::run(screen, queue)
+  loop do
+    Game.main_menu(screen)
+
+    Game.run(screen)
   end
 end
